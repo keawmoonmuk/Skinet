b855e74b348915c852bfb3d0401177a8cdd36e0e
kreangkrai keaw <kreangkrai.dev@gmail.com>
2021-03-10 09:42:29 +0700

    Test commit file and push

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -2,6 +2,7 @@ namespace Core.Entities
 {
     public class Product
     {
+        //create Id and create name 
         public int Id { get; set; }
         public string Name { get; set; }
         
